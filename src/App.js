f7eda926d52d17e11574948b1c1ad6a680d80046
paugarcia32 <paugarcia32@gmail.com>
2023-10-17 16:52:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [receivedMessage, setReceivedMessage] = useState('');
+
+  const handleSubmit = async () => {
+    try {
+      // Reemplaza 'URL_DEL_BACKEND' con la URL real de tu backend
+      const response = await fetch('URL_DEL_BACKEND', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message }),
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        setReceivedMessage(responseData.message);
+      } else {
+        setReceivedMessage('Error al recibir el mensaje');
+      }
+    } catch (error) {
+      console.error('Error al enviar el mensaje:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Aquí puedes agregar lógica adicional si lo necesitas cuando se monta el componente
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Formulario de Mensaje</h1>
+      <input
+        type="text"
+        placeholder="Escribe tu mensaje"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Enviar Mensaje</button>
+      <p>Mensaje recibido del backend: {receivedMessage}</p>
     </div>
   );
 }
